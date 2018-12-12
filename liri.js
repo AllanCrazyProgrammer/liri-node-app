0c2a2269ae0099174ef87201903e155bbb87a8f8
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2018-12-12 14:56:39 -0600

    spotify-this-song done.

--- liri.js
+++ liri.js
@@ -22,16 +22,32 @@ switch (commands) {
         axios.get(queryUrl).then(
             function (response) {
                 console.log("\n Name of the concert: " + response.data[1].venue.name);
-                
+
                 console.log("\n Location of the concert: " + response.data[1].venue.country + ", " + response.data[1].venue.city);
 
-                console.log("\n Concert date: " + moment( response.data[1].datetime).format( "MM-DD-YYYY"));
-
+                console.log("\n Concert date: " + moment(response.data[1].datetime).format("MM-DD-YYYY"));
             }
         )
-
-
         break;
+
+    case "spotify-this-song":
+
+        var song = process.argv[3];
+        var defaultSong = "The sign";
+
+        spotify.search({ type: 'track', query: song, limit: 5 }, function (err, data) {
+            if (err) {
+                spotify.search({ type: 'track', query: defaultSong, limit: 5 }, function (err, data) {
+                    console.log(data);
+                });
+            }
+
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Preview: " + data.tracks.items[0].preview_url);
+
+        });
 
     default:
         break;
